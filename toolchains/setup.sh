b90cad4cbe1372372d09138879b104f1a4828df8
ctxcode <vdvlorenz@gmail.com>
2025-03-29 07:07:59 +0100

    fix toolchain command

--- toolchains/setup.sh
+++ toolchains/setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-./setup-linux-x64.sh
-./setup-macos-x64.sh
-./setup-macos-arm64.sh
-./setup-win-x64.sh
+chmod +x ./toolchains/setup-linux-x64.sh
+./toolchains/setup-linux-x64.sh
+chmod +x ./toolchains/setup-macos-x64.sh
+./toolchains/setup-macos-x64.sh
+chmod +x ./toolchains/setup-macos-arm64.sh
+./toolchains/setup-macos-arm64.sh
+chmod +x ./toolchains/setup-win-x64.sh
+./toolchains/setup-win-x64.sh
 
 echo "# All toolchains & libraries are ready"
