68d71d758d899756c649e7134c5dcc3fc5382a2f
ctxcode <vdvlorenz@gmail.com>
2025-04-02 18:42:03 +0200

    tests

--- tests/compile-errors/run.sh
+++ tests/compile-errors/run.sh
@@ -2,11 +2,30 @@
 
 echo "# Test compile errors"
 
+# Set the filepath here
+FILEPATH="./tests/compile-errors/errors.txt"
 count=0
-for file in ./tests/compile-errors/*.valk; do
+
+# Check if the file exists
+if [ ! -f "$FILEPATH" ]; then
+    echo "Error: File '$FILEPATH' not found"
+    exit 1
+fi
+
+# Read the file line by line
+while IFS=';' read -r file msg; do
+    # Remove any trailing whitespace from var2
+    msg=$(echo "$msg" | xargs)
+
+    if [[ "$file" == "#"* ]]; then
+        continue  # Skips to next iteration
+    fi
+
     count=$((count+1))
-    cmd="./valk build $file -r"
+    cmd="./valk build ./tests/compile-errors/$file.valk"
     output=$($cmd)
+
+    # Check if build command failed
     if [[ $? -ne 1 ]]; then
         echo "# Build command should have failed, but didnt."
         echo "- File: $file"
@@ -14,7 +33,18 @@ for file in ./tests/compile-errors/*.valk; do
         echo "- Output: $output"
         exit 1
     fi
-done
+
+    # Check if error message is found in the output
+    if [[ "$output" != *"$msg"* ]]; then
+        echo "# Build command did return the correct error message."
+        echo "- File: $file"
+        echo "- Cmd: $cmd"
+        echo "- Output: $output"
+        echo "- Missing mesage: $msg"
+        exit 1
+    fi
+
+done < "$FILEPATH"
 
 echo "# All build commands have failed succesfully"
 echo "# Test count: $count"
