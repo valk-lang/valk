6dee63ea7f867955a26de8d564fb9af2444976c7
ctxcode <vdvlorenz@gmail.com>
2025-08-25 14:18:03 +0200

    json benchmarks

--- examples/bench/json/main.go
+++ examples/bench/json/main.go
@@ -40,7 +40,24 @@ func main() {
 
     elapsed := time.Since(start)
 
-    // Check
     fmt.Printf("Name: %s, Age: %d, Email: %s\n", p["name"], p["age"], p["email"])
 	fmt.Printf("Decoded %d times in %d ms\n", amount, int(elapsed / time.Millisecond))
+
+    // Encoding
+    start = time.Now()
+    str := ""
+
+    for i := amount; i > 0; i-- {
+        p = decode()
+        s, err := json.Marshal(p)
+        if err != nil {
+            fmt.Errorf("failed to encode JSON: %w", err)
+            break
+        }
+        str = string(s)
+    }
+
+    elapsed = time.Since(start)
+	fmt.Printf("Json string: %s\n", str)
+	fmt.Printf("Encoded %d times in %d ms\n", amount, int(elapsed / time.Millisecond))
 }
