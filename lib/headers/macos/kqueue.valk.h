ca728eef0bef265f99cb0c3d8196d27ece758061
ctxcode <vdvlorenz@gmail.com>
2026-01-16 09:41:04 +0100

    kqueue

---
+++ lib/headers/macos/kqueue.valk.h
@@ -0,0 +1,11 @@
+
+struct kEvent {
+    ident: uint	// identifier for this event
+    filter: i16 // filter for event
+    flags: u16 // action flags for kqueue
+    fflags: u32 // filter flag value
+    data: i64 // filter data value
+    udata: ptr // opaque user data identifier
+}
+
+fn kevent(kq: i32, changelist: ?*[<kEvent>], nchanges: i32, eventlist: ?*[<kEvent>], nevents: i32, timeout: libc_timespec) i32;
